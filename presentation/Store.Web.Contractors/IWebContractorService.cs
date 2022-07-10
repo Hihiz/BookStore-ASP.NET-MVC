4c5cede4a8ce707e788ab06c4bc2213fbe8c3351
Hihiz <22-49@bk.ru>
2022-07-11 01:08:14 +0300

    Реализовать оплату картой

---
+++ presentation/Store.Web.Contractors/IWebContractorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Store.Web.Contractors
+{
+    public interface IWebContractorService
+    {
+
+        string UniqueCode { get; }
+
+        string GetUri { get; }
+    }
+}
--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Store.Contractors;
 using Store.Messages;
+using Store.Web.Contractors;
 using Store.Web.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,18 +16,21 @@ namespace Store.Web.Controllers
         private readonly IOrderRepository orderRepository;
         private readonly IEnumerable<IDeliveryService> deliveryServices;
         private readonly IEnumerable<IPaymentService> paymentServices;
+        private readonly IEnumerable<IWebContractorService> webContractorServices;
         private readonly INotificationService notificationService;
 
         public OrderController(IBookRepository bookRepository,
                                IOrderRepository orderRepository,
                                IEnumerable<IDeliveryService> deliveryServices,
                                IEnumerable<IPaymentService> paymentServices,
+                               IEnumerable<IWebContractorService> webContractorServices,
                                INotificationService notificationService)
         {
             this.bookRepository = bookRepository;
             this.orderRepository = orderRepository;
             this.deliveryServices = deliveryServices;
             this.paymentServices = paymentServices;
+            this.webContractorServices = webContractorServices;
             this.notificationService = notificationService;
         }
 
@@ -261,6 +265,11 @@ namespace Store.Web.Controllers
 
             var form = paymentService.CreateForm(order);
 
+            var webContractorService = webContractorServices.SingleOrDefault(service => service.UniqueCode == uniqueCode);
+            if (webContractorService != null)
+                return Redirect(webContractorService.GetUri);
+
+
             return View("PaymentStep", form);
         }
 
@@ -281,6 +290,13 @@ namespace Store.Web.Controllers
             }
 
             return View("PaymentStep", form);
+        }
+
+        public IActionResult Finish()
+        {
+            HttpContext.Session.RemoveCart();
+
+            return View();
         }
     }
 }
