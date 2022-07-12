6dac9410a7368b435687f558d89fe1af77cc5bb5
Hihiz <22-49@bk.ru>
2022-07-12 20:52:11 +0300

    Исправить ошибки

---
+++ application/Store.Web.App/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace Store.Web.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public OrderItemModel[] Items { get; set; } = new OrderItemModel[0];
+        public int TotalCount { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public string CellPhone { get; set; }
+
+        public string DeliveryDescription { get; set; }
+
+        public string PaymentDescription { get; set; }
+
+        public Dictionary<string, string> Errors { get; set; } = new Dictionary<string, string>();
+    }
+}
--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -109,9 +109,8 @@ namespace Store.Web.Controllers
                 order = orderRepository.GetById(cart.OrderId);
             }
             else
-            {
-                order = orderRepository.Create();
-                cart = new Cart(order.Id);
+            {                
+                cart = new Cart(order.Id, 0, 0m);
             }
 
             return (order, cart);
@@ -121,8 +120,7 @@ namespace Store.Web.Controllers
         {
             orderRepository.Update(order);
 
-            cart.TotalCount = order.TotalCount;
-            cart.TotalPrice = order.TotalPrice;
+            cart = new Cart(order.Id, order.TotalCount, order.TotalPrice);
 
             HttpContext.Session.Set(cart);
         }
