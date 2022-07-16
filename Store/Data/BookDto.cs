28ebb713c006d1f5321a22a279652541e0977904
Hihiz <22-49@bk.ru>
2022-07-16 23:32:29 +0300

    Включить миграции

--- Store/Data/BookDto.cs
+++ Store/Data/BookDto.cs
@@ -3,15 +3,10 @@
     public class BookDto
     {
         public int Id { get; set; }
-
         public string Isbn { get; set; }
-
         public string Author { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
     }
 }
