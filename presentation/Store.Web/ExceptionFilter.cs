c32205914393779e1883fbc14f30df85d426726a
Hihiz <22-49@bk.ru>
2022-07-20 20:45:27 +0300

    Создать класс для обработки ошибок

---
+++ presentation/Store.Web/ExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public void OnException(ExceptionContext context)
+        {
+
+        }
+    }
+}
